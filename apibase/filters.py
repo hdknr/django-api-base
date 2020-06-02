883c1d264f29757973a8cf962ea9efbce4549e72
user.name <gmail@hdknr.com>
2020-06-02 17:46:44 +0900

    fitter_int

--- apibase/filters.py
+++ apibase/filters.py
@@ -6,3 +6,7 @@ import django_filters
 
 class BaseFilter(django_filters.FilterSet):
     pk = django_filters.NumberFilter(field_name='id')
+
+    def filter_int(self, queryset, name, value):
+        q = {name: int(round(value))}
+        return queryset.filter(**q)
