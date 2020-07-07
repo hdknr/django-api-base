03b933b41a022c5cbb927c321d9bf582fc78d465
user.name <gmail@hdknr.com>
2020-07-07 20:34:24 +0900

    permissios

---
+++ apibase/permissions.py
@@ -0,0 +1,43 @@
+from rest_framework import permissions
+from functools import wraps, partial
+from logging import getLogger
+logger = getLogger(__name__)
+
+
+
+def has_perms(func, permission, *args, **kwargs):
+    def wrapper(func):
+        @wraps(func)
+        def wrapped(self, info, *func_args, **func_kwargs):
+            if not info.context.user.has_perm(permission):
+                return None
+            return func(self, info, *func_args, **func_kwargs)
+        return wrapped
+    return wrapper
+
+
+class Permission(permissions.IsAuthenticated):
+    PERM_CODE = None
+    PRIVATE = True
+
+    @classmethod
+    def has(cls, func):
+        @wraps(func)
+        def wrapped(self, info, *func_args, **func_kwargs):
+            if not cls.check_info(info, *func_args, **func_kwargs):
+                return None
+            return func(self, info, *func_args, **func_kwargs)
+        return wrapped
+
+    @classmethod
+    def check_info(cls, info, *args, **kwargs):
+        return info.context.user.has_perm(cls.PERM_CODE)
+
+    def has_permission(self, request, view):
+        isvalid = False if self.PRIVATE else (request.method in permissions.SAFE_METHODS)
+        isvalid = isvalid or request.user.has_perm(self.PERM_CODE)
+        if not isvalid:
+            logger.info(f"{request.user} has not {self.PERM_CODE}")
+        return isvalid
+
+
