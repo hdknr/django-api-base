66cb1d0630c5525ed5d3675ab64e8b53f36a79b8
user.name <gmail@hdknr.com>
2020-08-21 20:30:31 +0900

    create multiple objects at one time

--- apibase/viewsets.py
+++ apibase/viewsets.py
@@ -1,7 +1,22 @@
 from rest_framework import (
-    viewsets, response, decorators)
+    viewsets, response, decorators, status)
+from rest_framework.response import Response
 from .paginations import Pagination
 
 
 class BaseModelViewSet(viewsets.ModelViewSet):
     pagination_class = Pagination
+
+    def get_serializer(self, *args, **kwargs):
+        if self.action == 'batch_create' and self.request.POST:
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
+
+    @decorators.action(methods=['post'], detail=False)
+    def batch_create(self, request, *args, **kwargs):
+        # only 'many=True' is added to super().create()
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
