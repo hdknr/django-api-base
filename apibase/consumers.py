14f2b3cda71bb2dad878514f5cfb209b38fe5385
user.name <gmail@hdknr.com>
2020-09-23 11:08:12 +0900

    create_schema_consumer

--- apibase/consumers.py
+++ apibase/consumers.py
@@ -10,3 +10,10 @@ class GraphqlWsConsumer(channels_graphql_ws.GraphqlWsConsumer):
     async def on_connect(self, payload):
         """New client connection handler."""
         logger.debug(f"on_connect ---------------{self.channel_name} {payload}")
+
+    @classmethod
+    def schema_consumer_class(cls, schema, name="SchemaWsConsumer"):
+        return type(name, (cls,), {"schema": schema})
+
+
+create_schema_consumer = GraphqlWsConsumer.schema_consumer_class
