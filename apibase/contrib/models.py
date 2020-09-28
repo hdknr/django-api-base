529238c4175004955c770069efd8b809ad8e7d09
user.name <gmail@hdknr.com>
2020-09-28 11:34:21 +0900

    typo

--- apibase/contrib/models.py
+++ apibase/contrib/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.auth import get_user_model as USER
-from django.contrib.contenttype.models import ContentType
+from django.contrib.contenttypes.models import ContentType
 
 
 User = USER()
