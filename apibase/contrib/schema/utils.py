7a1e3027116805d0eeec3d8c10846d86be22186a
user.name <gmail@hdknr.com>
2020-09-28 15:12:42 +0900

    export, import Groups + Permission

---
+++ apibase/contrib/schema/utils.py
@@ -0,0 +1,45 @@
+from . import exec_query
+from .. import models
+import json
+
+
+def export_groups():
+    q = '''
+        query GroupSet {
+            group_set {
+                edges {
+                    node {
+                        pk, name
+                        permissions {
+                            edges {
+                                node {
+                                    pk, codename, name
+                                    content_type {
+                                        app_label, model
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        '''
+    res = exec_query(q)
+    return json.dumps(res)
+
+
+def import_groups(path):
+    data = json.load(open(path))
+    for edge in data['group_set']['edges']:
+        group, _ = models.Group.objects.get_or_create(
+            name=edge['node']['name'])
+        for edge2 in edge['node']['permissions']['edges']:
+            defaults = dict(name=edge2['node']['name'])
+            content_type = models.ContentType.objects.filter(
+                **edge2['node']['content_type']).first()
+            if content_type:
+                permission, _ = models.Permission.objects.update_or_create(
+                    defaults,
+                    codename=edge2['node']['codename'], content_type=content_type)
+                group.permissions.add(permission)
