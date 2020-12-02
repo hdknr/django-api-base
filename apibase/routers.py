1eed51b284023635492eb030a4c8d6f9701a656f
hdknr <gmail@hdknr.com>
2020-12-02 15:10:25 +0900

    basename if queryset is not given

--- apibase/routers.py
+++ apibase/routers.py
@@ -6,13 +6,12 @@ class DefaultRouter(DrfDefaultRouter):
     def get_default_basename(self, viewset):
         """ Generate defautl 'basename' """
         queryset = getattr(viewset, 'queryset', None)
-
-        assert queryset is not None, '`basename` argument not specified, and could ' \
-            'not automatically determine the name from the viewset, as ' \
-            'it does not have a `.queryset` attribute.'
-
-        object_name = queryset.model._meta.object_name.lower()
-        app_label = queryset.model._meta.app_label.lower()
+        if queryset is None:
+            app_label = viewset.__module__.split('.')[0]
+            object_name = viewset.__name__.lower().replace('viewset', '')
+        else:
+            object_name = queryset.model._meta.object_name.lower()
+            app_label = queryset.model._meta.app_label.lower()
         return f"api-{app_label}-{object_name}" 
 
 
