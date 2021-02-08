16dec46442c7d37af403050634b1359bc518b7a7
user.name <gmail@hdknr.com>
2021-02-08 20:07:59 +0900

    Batch Update

--- apibase/serializers.py
+++ apibase/serializers.py
@@ -3,10 +3,11 @@ from django.http import QueryDict
 from django.db.models import Model
 from django.db.models.fields.reverse_related import OneToOneRel
 from django.contrib.sites.shortcuts import get_current_site
-from rest_framework import fields, serializers
+from rest_framework import fields, serializers, exceptions
 from rest_framework.fields import empty
 from .settings import apibase_settings
 import re
+import inspect
 
 
 def to_urn(instance, nss=None, nid=None):
@@ -15,78 +16,74 @@ def to_urn(instance, nss=None, nid=None):
     return f"urn:{nid}:{nss}:{instance._meta.app_label}:{instance._meta.model_name}:{instance.pk}"
 
 
-def endpoint_from_urn(urn, domain=None, nid=None, prefix='/api/rest', request=None):
+def endpoint_from_urn(urn, domain=None, nid=None, prefix="/api/rest", request=None):
     nid = nid or apibase_settings.URN_NID
-    domain = domain or apibase_settings.DOMAIN or get_current_site(
-        request).domain
+    domain = domain or apibase_settings.DOMAIN or get_current_site(request).domain
     ma = re.findall(r"([^:]+)", urn)
-    if len(ma) == 6 and ma[0] == 'urn' and ma[1] == nid:
-        if ma[2] == 'self':
-            service = ''
+    if len(ma) == 6 and ma[0] == "urn" and ma[1] == nid:
+        if ma[2] == "self":
+            service = ""
         else:
-            _, domain = re.search(r'(?:([^\.]+)\.)?(.+)', domain).groups()
+            _, domain = re.search(r"(?:([^\.]+)\.)?(.+)", domain).groups()
             service = ma[2] + "."
         return f"{apibase_settings.SCHEME}://{service}{domain}{prefix}/{ma[3]}/{ma[4]}/{ma[5]}/"
     return None
 
 
-def drf_endpoint(instance, url_name=None, pk_name='pk'):
-    ''' DRF endpoint '''
+def drf_endpoint(instance, url_name=None, pk_name="pk"):
+    """ DRF endpoint """
     try:
-        if hasattr(instance, 'get_endpoint_url'):
+        if hasattr(instance, "get_endpoint_url"):
             return instance.get_endpoint_url()
-        name = url_name or f"api-{instance._meta.app_label}-{instance._meta.model_name}-detail"
+        name = (
+            url_name
+            or f"api-{instance._meta.app_label}-{instance._meta.model_name}-detail"
+        )
         return reverse(name, kwargs={pk_name: instance.pk})
     except:
         pass
-    return ''
+    return ""
 
 
 class EndpointField(fields.Field):
-
     def __init__(self, **kwargs):
-        kwargs['source'] = '*'
-        kwargs['read_only'] = True
-        self.url_name = kwargs.get('url_name', None)
-        self.attr_name = kwargs.get('attr_name', None)
+        kwargs["source"] = "*"
+        kwargs["read_only"] = True
+        self.url_name = kwargs.get("url_name", None)
+        self.attr_name = kwargs.get("attr_name", None)
         super().__init__(**kwargs)
 
     def get_url_name(self, value):
         return self.url_name
 
     def to_representation(self, value):
-        instance = self.attr_name and getattr(
-            value, self.attr_name, None) or value
+        instance = self.attr_name and getattr(value, self.attr_name, None) or value
         url = drf_endpoint(instance, url_name=self.get_url_name(value))
-        request = self.context.get('request', None)
+        request = self.context.get("request", None)
         return (request and url) and request.build_absolute_uri(url) or url or None
 
 
 class UrnField(fields.Field):
-
     def __init__(self, **kwargs):
-        kwargs['source'] = '*'
-        kwargs['read_only'] = True
-        self.attr_name = kwargs.get('attr_name', None)
+        kwargs["source"] = "*"
+        kwargs["read_only"] = True
+        self.attr_name = kwargs.get("attr_name", None)
         super().__init__(**kwargs)
 
     def to_representation(self, value):
-        instance = self.attr_name and getattr(
-            value, self.attr_name, None) or value
+        instance = self.attr_name and getattr(value, self.attr_name, None) or value
         return to_urn(instance)
 
 
 class DisplayField(fields.Field):
-
     def __init__(self, **kwargs):
-        kwargs['source'] = '*'
-        kwargs['read_only'] = True
-        self.attr_name = kwargs.get('attr_name', None)
+        kwargs["source"] = "*"
+        kwargs["read_only"] = True
+        self.attr_name = kwargs.get("attr_name", None)
         super().__init__(**kwargs)
 
     def to_representation(self, value):
-        instance = self.attr_name and getattr(
-            value, self.attr_name, None) or value
+        instance = self.attr_name and getattr(value, self.attr_name, None) or value
         return str(instance)
 
 
@@ -100,23 +97,21 @@ class BaseModelSerializer(serializers.ModelSerializer):
 
     @property
     def children_set(self):
-        return getattr(self, '_children_set', {})
+        return getattr(self, "_children_set", {})
 
     def get_children(self, name):
         return self.children_set.get(name, [])
 
     def to_representation(self, instance):
-        '''(override)'''
+        """(override)"""
         data = super().to_representation(instance)
-        if hasattr(self, 'patch_result'):
+        if hasattr(self, "patch_result"):
             self.patch_result(instance, data)
         return data
 
     def run_validation_querydict(self, data=empty):
-        self._children_set = QueryDict(
-            '', mutable=True, encoding=data.encoding)
-        copied = QueryDict(
-            '', mutable=True, encoding=data.encoding)
+        self._children_set = QueryDict("", mutable=True, encoding=data.encoding)
+        copied = QueryDict("", mutable=True, encoding=data.encoding)
 
         for key, value in data.lists():
             if key in self.nested_fields:
@@ -128,7 +123,7 @@ class BaseModelSerializer(serializers.ModelSerializer):
         return super().run_validation(data=data)
 
     def run_validation(self, data=empty):
-        '''(override)'''
+        """(override)"""
         if data == empty:
             return super().run_validation(data=data)
 
@@ -136,8 +131,7 @@ class BaseModelSerializer(serializers.ModelSerializer):
             if isinstance(data, QueryDict):
                 return self.run_validation_querydict(data=data)
             self._children_set = dict(
-                (i, data.pop(i, None))
-                for i in self.nested_fields
+                (i, data.pop(i, None)) for i in self.nested_fields
             )
 
         return super().run_validation(data=data)
@@ -145,8 +139,7 @@ class BaseModelSerializer(serializers.ModelSerializer):
     @classmethod
     def update_or_create(cls, partial=None, id=None, **validated_data):
         instance = id and cls.Meta.model.objects.filter(id=id).first()
-        serializer = cls(instance=instance,
-                         data=validated_data, partial=partial)
+        serializer = cls(instance=instance, data=validated_data, partial=partial)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
@@ -157,7 +150,7 @@ class BaseModelSerializer(serializers.ModelSerializer):
         if not children or not instance:
             return
 
-        name = re.sub(r'(.+)(_set)$', r'\g<1>', field_name)
+        name = re.sub(r"(.+)(_set)$", r"\g<1>", field_name)
         related_field = self.Meta.model._meta.get_field(name)
         remote_field_name = related_field.remote_field.name
         if isinstance(related_field, OneToOneRel):
@@ -183,9 +176,10 @@ class BaseModelSerializer(serializers.ModelSerializer):
             self.update_nested(instance, validated_data, field_name, children)
 
     def validated_children_set(self, validated_data):
-        children_set = getattr(self, '_children_set', [])
-        children_set = children_set or \
-            dict((i, validated_data.pop(i, [])) for i in self.nested_fields)
+        children_set = getattr(self, "_children_set", [])
+        children_set = children_set or dict(
+            (i, validated_data.pop(i, [])) for i in self.nested_fields
+        )
         return children_set
 
     def update(self, instance, validated_data):
@@ -202,3 +196,55 @@ class BaseModelSerializer(serializers.ModelSerializer):
 
 class UrnModelSerializer(BaseModelSerializer):
     urn = UrnField()
+
+
+class BatchSerializerMixin:
+    def to_internal_value(self, data):
+        ret = super().to_internal_value(data)
+        id_attr = getattr(self.Meta, "update_lookup_field", "id")
+        request_method = getattr(
+            getattr(self.context.get("view"), "request"), "method", ""
+        )
+
+        if all(
+            (
+                isinstance(self.root, BatchListSerializer),
+                id_attr,
+                request_method in ("PUT", "PATCH"),
+            )
+        ):
+            id_field = self.fields[id_attr]
+            id_value = id_field.get_value(data)
+
+            ret[id_attr] = id_value
+
+        return ret
+
+
+class BatchListSerializer(serializers.ListSerializer):
+    update_lookup_field = "id"
+
+    def update(self, queryset, all_validated_data):
+        id_attr = getattr(self.child.Meta, "update_lookup_field", "id")
+
+        updating = {i.pop(id_attr): i for i in all_validated_data}
+
+        if not all((bool(i) and not inspect.isclass(i) for i in updating.keys())):
+            raise exceptions.ValidationError("")
+
+        objects_to_update = queryset.filter(
+            **{"{}__in".format(id_attr): updating.keys(),}
+        )
+
+        if len(updating) != objects_to_update.count():
+            raise exceptions.ValidationError("Could not find all objects to update.")
+
+        updated_objects = []
+
+        for instance in objects_to_update:
+            obj_id = getattr(instance, id_attr)
+            obj_validated_data = updating.get(obj_id)
+
+            updated_objects.append(self.child.update(instance, obj_validated_data))
+
+        return updated_objects
--- apibase/viewsets.py
+++ apibase/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import (viewsets, decorators, status)
+from rest_framework import viewsets, decorators, status
 from rest_framework.response import Response
 from django.contrib.auth.models import Permission
 from . import paginations, permissions
@@ -8,25 +8,51 @@ class BaseModelViewSet(viewsets.ModelViewSet):
     pagination_class = paginations.Pagination
 
     def get_serializer(self, *args, **kwargs):
-        if self.action == 'batch_create' and self.request.POST:
-            kwargs['many'] = True
+        if self.action == "batch_create" and self.request.POST:
+            kwargs["many"] = True
         return super().get_serializer(*args, **kwargs)
 
-    @decorators.action(methods=['post'], detail=False)
+    @decorators.action(methods=["post"], detail=False)
     def batch_create(self, request, *args, **kwargs):
         # only 'many=True' is added to super().create()
         serializer = self.get_serializer(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     @classmethod
     def permissions(cls):
         return [
             Permission.objects.filter(
-                **dict(zip(('content_type__app_label', 'codename'), p.PERM_CODE.split('.')))
+                **dict(
+                    zip(("content_type__app_label", "codename"), p.PERM_CODE.split("."))
+                )
             ).first()
             for p in cls.permission_classes
             if issubclass(p, permissions.Permission) and p.PERM_CODE
         ]
+
+    def update(self, request, *args, **kwargs):
+        """(overrid)"""
+        many = kwargs.pop("many", False)
+        if not many:
+            # normal
+            return super().update(request, *args, **kwargs)
+
+        # batch update
+        # Serlialier : list_serializer_class = BatchListSerializer
+        partial = kwargs.pop("partial", False)
+        serializer = self.get_serializer(
+            self.filter_queryset(self.get_queryset()),
+            data=request.data,
+            many=True,
+            partial=partial,
+        )
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
