ecd0a855a2390beae93fbd3d43fe1729a6612fa2
user.name <gmail@hdknr.com>
2021-11-26 20:02:13 +0900

    remove FilteringNode

--- apibase/graphql/nodes.py
+++
@@ -1,9 +0,0 @@
-from graphene.relay import Node
-
-from .fields import NodeSet
-
-
-class FilteringNode(Node):
-    @classmethod
-    def Field(cls, *args, **kwargs):  # noqa: N802
-        return NodeSet(cls, *args, **kwargs)  # NodeField を返している
